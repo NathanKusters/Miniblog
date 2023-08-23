d6537ff0ab4f46ab4541894e2d4bf679b50bc45b
Nathan Küsters <nathankusterss@gmail.com>
2023-08-23 20:58:07 -0300

    config Firebase

---
+++ firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcB1OO1mmziZ0EHnoyNfLWQpAhiiqyMSU",
+  authDomain: "miniblog-3d50c.firebaseapp.com",
+  projectId: "miniblog-3d50c",
+  storageBucket: "miniblog-3d50c.appspot.com",
+  messagingSenderId: "321449792335",
+  appId: "1:321449792335:web:c7362d329c8aa283f16b3a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
