02324c4f5bc7e18a1052c0f700bd838074b64620
Nathan Küsters <nathankusterss@gmail.com>
2023-08-23 22:57:46 -0300

    addnavbar and footer with CSS

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -13,12 +15,14 @@ function App() {
   return (
     <>
     <BrowserRouter> 
+    <NavBar />
       <div className='container'>
         <Routes> 
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
         </Routes>
       </div>
+    <Footer />
     </BrowserRouter>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+const footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3>Escreva sobre oque você tem interesse!</h3>
+      <p>Mini Blog &copy; 2023</p>
+    </footer>
+  )
+}
+
+export default footer
