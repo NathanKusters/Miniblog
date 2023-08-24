048d3c8d9c5f4795e6fc895b955e4b675d002c55
Nathan Küsters <nathankusterss@gmail.com>
2023-08-23 21:24:12 -0300

    config React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+
+// pages
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+
+
 function App() {
 
   return (
     <>
+    <BrowserRouter> 
       <div className='container'>
-        <h1>MiniBlog</h1>
+        <Routes> 
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
       </div>
+    </BrowserRouter>
     </>
   )
 }
