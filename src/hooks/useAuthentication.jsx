eb8bcda02cf5637ebb393d691bbf38f3026e327d
Nathan Küsters <nathankusterss@gmail.com>
2023-08-24 22:13:57 -0300

    Adding User Register

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -1,3 +1,5 @@
+import { db } from "../firebase/config"; 
+
 import {
     getAuth,
     createUserWithEmailAndPassword,
@@ -23,4 +25,59 @@ export const useAuthentication = () => {
             return;
         }
     }
+
+    const createUser = async (data) => {
+        checkIfIsCancelled()
+
+        setLoading(true);
+        setError(null);
+
+        try {
+
+            const {user} = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            )
+
+            await updateProfile(user, {
+                displayName: data.displayName
+            });
+
+            setLoading(false);
+
+            return user;
+            
+        } catch (error) {
+
+            console.log(error.message);
+            console.log(typeof error.message);
+
+            let systemErrorMessage;
+            // por algum motivo não está funcionado
+            if(error.message.includes("Password")) {
+                systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres"
+            // por algum motivo não está funcionado
+            } else if (error.message.includes("email-already")) {
+                systemErrorMessage = "E-mail já cadastrado";
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde ( A senha precisa conter pelos menos 6 caracteres, ou o E-mail já cadastrado)";
+            }
+
+            setLoading(false);
+            setError(systemErrorMessage);
+        }
+
+    };
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, []);
+
+    return {
+        auth,
+        createUser,
+        error,
+        loading,
+    };
 };
