af6c681c279a2c219ad9d534605bf2e08a290a18
Nathan Küsters <nathankusterss@gmail.com>
2023-09-04 22:45:24 -0300

    Mostrando links para usuários autenticados

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import { NavLink } from "react-router-dom";
 
+import { useAuthentication } from "../hooks/useAuthentication";
+
+import { useAuthValue } from "../context/AuthContext";
+
 import styles from "./Navbar.module.css";
 
 const NavBar = () => {
+
+  const { user } = useAuthValue();
+
+
+
   return (
     <nav className={styles.navbar}>
       <NavLink to={"/"} className={styles.brand}>
@@ -12,12 +21,26 @@ const NavBar = () => {
         <li>
           <NavLink to="/" className={({isActive}) => (isActive ? styles.active : "")}>Home</NavLink>
         </li>
-        <li>
-          <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>Entrar</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>Cadastrar</NavLink>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>Entrar</NavLink>
+            </li>
+            <li>
+              <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>Cadastrar</NavLink>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>
+              <NavLink to="/posts/create" className={({isActive}) => (isActive ? styles.active : "")}>Novo Post</NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard" className={({isActive}) => (isActive ? styles.active : "")}>Dashboard</NavLink>
+            </li>
+          </>
+        )}
         <li>
           <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : "")}>Sobre</NavLink>
         </li>
