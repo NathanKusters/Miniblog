715b557c8dec229ca217f7835e870ac9cb1c43bd
Nathan Küsters <nathankusterss@gmail.com>
2023-09-04 23:02:09 -0300

    bloqueando página que precisam de autenticação e adicionando conteudo para o About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,19 @@
 // CSS
 import styles from "./About.module.css";
 
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
-    <h1>About</h1>
+    <div  className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span> 
+      </h2>
+      <p>Este Projeto consiste em um blog feito em React no Front-end e Firebase no Back-end.</p>
+      <Link to="/posts/create" className="btn">
+        Criar Post
+      </Link>
+    </div>
   )
 }
 
