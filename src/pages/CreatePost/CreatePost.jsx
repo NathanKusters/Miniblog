5869a251749c80a9c9e4dc341de6b22f1b519265
Nathan Küsters <nathankusterss@gmail.com>
2023-09-06 19:21:07 -0300

    fazendo a validação dos dados

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -11,32 +11,47 @@ const CreatePost = () => {
   const [image, setImage] = useState("");
   const [body, setBody] = useState("");
   const [tags, setTags] = useState([]);
-  const [formError, setformError] = useState("");
+  const [formError, setFormError] = useState("");
 
-  const {user} = useAuthValue();
+  const { user } = useAuthValue();
 
-  const {insertDocument, response} = useInsertDocument("posts");
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setformError("");
+    setFormError("");
 
     // validate image URL
 
+    try {
+      new URL(image)
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
+
     // criar o array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
 
     // checar todos os valores
+    if(!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    if (formError) return;
 
     insertDocument({
       title,
       image,
       body,
-      tags,
+      tagsArray,
       uid: user.uid,
       createBy: user.displayName,
     })
 
     // redirect to home page
+    navigate("/");
   }
 
 
@@ -92,13 +107,18 @@ const CreatePost = () => {
             <button className="btn">
               Cadastrar
             </button>}
-          {response.loading && 
+          {response.loading && (
             <button className="btn" disabled>
               Aguarde...
-            </button>}
+            </button>
+          )}
           {response.error && 
             <p className="error">
               {response.error}
+            </p>}
+            {formError && 
+            <p className="error">
+              {formError}
             </p>}
         </form>
     </div>
